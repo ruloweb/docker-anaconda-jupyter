de21ae9741e0d6b16708ccd8e3c07af82fd46cc9
Jose Sanchez <josegabriel.st@gmail.com>
2017-05-21 18:44:25 +0000

    Run cron when container starts.

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Cron process
+cron -f 2>&1 &
+
+# Set Jupyter password
 sed -ie "s/#c.NotebookApp.password = u''/c.NotebookApp.password = u'$JUPYTER_PASSWORD'/g" ~/.jupyter/jupyter_notebook_config.py
 
-/opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser
+exec /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser
